ba859c208411901b5d8429d33711828aa71065db
Htet Lin <htetlin099798@gmail.com>
2024-02-23 14:59:38 +0630

    style: add Inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
-		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
 
-		// Or if using `src` directory:
-		"./src/**/*.{js,ts,jsx,tsx,mdx}",
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
 
-		"node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-	],
-	theme: {
-		extend: {
-			flex: {
-				fluid: "1 0 auto",
-				row_fulid: "1 auto",
-				row_auto: "0 0 auto",
-				root: "1",
-			},
-		},
-	},
-	plugins: [require("flowbite/plugin")],
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      flex: {
+        fluid: "1 0 auto",
+        row_fulid: "1 auto",
+        row_auto: "0 0 auto",
+        root: "1",
+      },
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
 };
