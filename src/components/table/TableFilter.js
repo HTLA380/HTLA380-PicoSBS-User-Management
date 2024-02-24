47957f1706bed3f9165b46e8315239dc80d4966e
Htet Lin <htetlin099798@gmail.com>
2024-02-24 14:32:03 +0630

    style: change tableFilter button styles

--- src/components/table/TableFilter.js
+++ src/components/table/TableFilter.js
@@ -2,32 +2,44 @@ import React from "react";
 import Button from "../form/button/Button";
 import Input from "../form/input/Input";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FaFilter } from "react-icons/fa";
+import { GoPlus } from "react-icons/go";
+import { PiExportBold } from "react-icons/pi";
 
 const TableFilter = ({ placeholder }) => {
-	return (
-		<>
-			<div className="flex items-center justify-between flex-wrap gap-4 px-8 pt-8">
-				<div className="flex items-center">
-					<Input leftIcon={faSearch} placeholder={placeholder} />
-				</div>
-				<div className="flex items-center gap-4">
-					<div>
-						<Button className="bg-[#dae4f1] text-[#3e97ff] hover:bg-[#3e97ff] hover:text-white">
-							Filter
-						</Button>
-					</div>
-					<div>
-						<Button className="bg-[#dae4f1] text-[#3e97ff] hover:bg-[#3e97ff] hover:text-white">
-							Export
-						</Button>
-					</div>
-					<div>
-						<Button className="text-white">Add User</Button>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="flex flex-wrap items-center justify-between gap-4 px-8 pt-8 font-medium">
+        <div className="flex items-center">
+          <Input
+            leftIcon={faSearch}
+            placeholder={placeholder}
+            className="bg-gray-100 border-none"
+          />
+        </div>
+        <div className="flex items-center gap-4">
+          <div>
+            <button className="flex items-center gap-2 px-6 py-3 text-sm text-blue-500 rounded-md bg-blue-50 hover:bg-blue-500 hover:text-white">
+              <FaFilter />
+              Filter
+            </button>
+          </div>
+          <div>
+            <button className="flex items-center gap-2 px-6 py-3 text-sm text-blue-500 rounded-md bg-blue-50 hover:bg-blue-500 hover:text-white">
+              <PiExportBold size={15} />
+              Export
+            </button>
+          </div>
+          <div>
+            <button className="flex items-center gap-2 px-6 py-3 text-sm text-white bg-blue-500 rounded-md">
+              <GoPlus size={20} />
+              Create User
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TableFilter;
