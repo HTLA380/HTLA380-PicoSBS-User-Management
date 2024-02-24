12e095be43f2ef665eb56fa04fe62733f5242e59
Htet Lin <htetlin099798@gmail.com>
2024-02-24 14:31:35 +0630

    chore: remove unnecessary elements

--- src/components/table/CustomTable.js
+++ src/components/table/CustomTable.js
@@ -6,186 +6,125 @@ import Image from "next/image";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 const CustomTable = ({ tableHeader, rowData }) => {
-	const [checked, setChecked] = useState(false);
-	const handleCheck = (e) => {
-		const { checked } = e.target;
-		setChecked(checked);
-	};
-	const sortHandler = () => {
-		console.log("sort");
-	};
-	return (
-		<div className="text-sm flex flex-col gap-3 lg:w-full w-11/12 customize__scroll m-auto px-4 bg-white">
-			<table className="table w-full">
-				<thead>
-					<tr className="text-left text-gray-400 uppercase border-b border-dashed border-gray-200">
-						<th className="px-3 py-4 min-w-[10px]">
-							<Checkbox checked={checked} onChange={handleCheck} />
-						</th>
-						{tableHeader.map((th, index) => (
-							<React.Fragment key={index}>
-								<th
-									className="px-3 py-4 text-left min-w-[125px] group cursor-pointer"
-									onClick={() => sortHandler()}
-								>
-									<div className="flex items-center text-xs">
-										{th}
-										<FontAwesomeIcon
-											icon={faChevronDown}
-											className="ml-2 w-[11px] h-[11px] opacity-0 transition duration-300 group-hover:opacity-100"
-										/>
-									</div>
-								</th>
-							</React.Fragment>
-						))}
-						<th className="px-3 py-4 text-left">Actions</th>
-					</tr>
-				</thead>
-				<tbody>
-					{rowData.length > 0 ? (
-						rowData.map((row, idx) => {
-							const rowKey = Object.keys(row);
-							return (
-								<tr
-									key={idx}
-									className="capitalize border-b border-dashed border-gray-200"
-								>
-									<td className="px-3 py-3 min-w-[10px]">
-										<Checkbox
-											onChange={handleCheck}
-											value={idx + 1}
-											// id={index + 1}
-											// name={index + 1}
-											// checked={allChecked}
-										/>
-									</td>
-									{rowKey.map((key, idx) =>
-										key === "customer" ? (
-											<td
-												className="text-left p-3 min-w-[200px]"
-												key={key + idx}
-											>
-												<div className="flex items-center">
-													<div
-														className={`w-[50px] h-[50px] cursor-pointer  rounded-full overflow-hidden mr-4`}
-													>
-														{row[key].img ? (
-															<Image
-																src={row[key].img}
-																alt=""
-																className="w-full h-full"
-																width={100}
-																height={100}
-															/>
-														) : (
-															<div className="text-base">
-																{row[key].name.charAt(0)}
-															</div>
-														)}
-													</div>
-													<div>{row[key].name}</div>
-												</div>
-											</td>
-										) : key === "product" ? (
-											<td
-												className="text-left p-3 min-w-[200px]"
-												key={key + idx}
-											>
-												<div className="flex items-center">
-													<div
-														className={`w-[50px] h-[50px] cursor-pointer ${
-															row[key].img
-																? "object-center"
-																: `text-center flex items-center justify-center`
-														} rounded-md overflow-hidden mr-4`}
-													>
-														{row[key].img ? (
-															<Image
-																src={row[key].img}
-																alt=""
-																className="w-full h-full"
-																width={100}
-																height={100}
-															/>
-														) : (
-															<div className="text-base">
-																{row[key].name.charAt(0)}
-															</div>
-														)}
-													</div>
-													<div>{row[key].name}</div>
-												</div>
-											</td>
-										) : key === "user" ? (
-											<td
-												className="text-left p-3 min-w-[200px]"
-												key={key + idx}
-											>
-												<div className="flex items-center">
-													<div
-														className={`w-[50px] h-[50px] cursor-pointer ${
-															row[key].img
-																? "object-center"
-																: `text-center flex items-center justify-center`
-														}  rounded-full overflow-hidden mr-4`}
-													>
-														{row[key].img ? (
-															<Image
-																src={row[key].img}
-																alt=""
-																className="w-full h-full"
-																width={100}
-																height={100}
-															/>
-														) : (
-															<div className="text-base">
-																{row[key].name.charAt(0)}
-															</div>
-														)}
-													</div>
-													<div className="flex flex-col gap-1">
-														<div>{row[key].name}</div>
-														<div className="text-xs text-gray-500">
-															{row[key].email}
-														</div>
-													</div>
-												</div>
-											</td>
-										) : key === "status" ? (
-											<td
-												key={key + idx}
-												className="p-3 text-left text-xs font-medium"
-											>
-												<div
-													className={`w-[76px] text-center text-xs font-medium px-2 py-1 capitalize rounded`}
-												>
-													{row[key]}
-												</div>
-											</td>
-										) : (
-											<td
-												key={key + idx}
-												className="text-left p-3 text-gray-600"
-											>
-												{row[key]}
-											</td>
-										)
-									)}
-									<td className="p-3">
-										<MenuDropdown />
-									</td>
-								</tr>
-							);
-						})
-					) : (
-						<tr>
-							<td>No data available in table</td>
-						</tr>
-					)}
-				</tbody>
-			</table>
-		</div>
-	);
+  const [checked, setChecked] = useState(false);
+  const handleCheck = (e) => {
+    const { checked } = e.target;
+    setChecked(checked);
+  };
+  const sortHandler = () => {
+    console.log("sort");
+  };
+  return (
+    <div className="flex flex-col w-11/12 gap-3 px-4 m-auto text-sm bg-white lg:w-full customize__scroll">
+      <table className="table w-full">
+        <thead>
+          <tr className="text-left text-gray-400 uppercase border-b border-gray-200 border-dashed">
+            <th className="px-3 py-4 min-w-[10px]">
+              <Checkbox checked={checked} onChange={handleCheck} />
+            </th>
+            {tableHeader.map((th, index) => (
+              <React.Fragment key={index}>
+                <th
+                  className="px-3 py-4 text-left min-w-[125px] group cursor-pointer"
+                  onClick={() => sortHandler()}>
+                  <div className="flex items-center text-xs font-semibold">
+                    {th}
+                    <FontAwesomeIcon
+                      icon={faChevronDown}
+                      className="ml-2 w-[11px] h-[11px] opacity-0 transition duration-300 group-hover:opacity-100"
+                    />
+                  </div>
+                </th>
+              </React.Fragment>
+            ))}
+            <th className="px-3 py-4 text-xs font-semibold text-left">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {rowData.length > 0 ? (
+            rowData.map((row, idx) => {
+              const rowKey = Object.keys(row);
+              return (
+                <tr
+                  key={idx}
+                  className="capitalize border-b border-gray-200 border-dashed">
+                  <td className="px-3 py-3 min-w-[10px]">
+                    <Checkbox
+                      onChange={handleCheck}
+                      value={idx + 1}
+                      // id={index + 1}
+                      // name={index + 1}
+                      // checked={allChecked}
+                    />
+                  </td>
+                  {rowKey.map((key, idx) =>
+                    key === "user" ? (
+                      <td
+                        className="text-left p-3 min-w-[200px]"
+                        key={key + idx}>
+                        <div className="flex items-center">
+                          <div
+                            className={`w-[50px] h-[50px] cursor-pointer ${
+                              row[key].img
+                                ? "object-center"
+                                : `text-center flex items-center justify-center`
+                            }  rounded-full overflow-hidden mr-4`}>
+                            {row[key].img ? (
+                              <Image
+                                src={row[key].img}
+                                alt=""
+                                className="w-full h-full"
+                                width={100}
+                                height={100}
+                              />
+                            ) : (
+                              <div className="text-base">
+                                {row[key].name.charAt(0)}
+                              </div>
+                            )}
+                          </div>
+                          <div className="flex flex-col gap-1">
+                            <div>{row[key].name}</div>
+                            <div className="text-xs text-gray-500">
+                              {row[key].email}
+                            </div>
+                          </div>
+                        </div>
+                      </td>
+                    ) : key === "status" ? (
+                      <td
+                        key={key + idx}
+                        className="p-3 text-xs font-medium text-left">
+                        <div
+                          className={`w-[76px] text-center text-xs font-medium px-2 py-1 capitalize rounded`}>
+                          {row[key]}
+                        </div>
+                      </td>
+                    ) : (
+                      <td
+                        key={key + idx}
+                        className="p-3 text-left text-gray-600">
+                        {row[key]}
+                      </td>
+                    )
+                  )}
+                  <td className="p-3">
+                    <MenuDropdown />
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr>
+              <td>No data available in table</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default CustomTable;
--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -4,31 +4,25 @@ import { UsersListInfos } from "constants/data";
 import React from "react";
 import TableFilter from "@/components/table/TableFilter";
 import TablePagination from "@/components/table/TablePagination";
-import Breadcrumbs from "@/components/Breadcrumbs";
 
-const tableHeader = ["User", "Role", "Last Login", "Two-Step", "Joined Date"];
+const tableHeader = ["User", "Username", "Role", "Status"];
+
 const UsersList = () => {
-	return (
-		<Layout>
-			<div className="bg-white rounded-lg pb-5">
-				<div className="px-8 pt-6">
-					<h1 className="font-semibold">Users List</h1>
-					<div>
-						<Breadcrumbs />
-					</div>
-				</div>
-				<div className="rounded-xl m-8 border">
-					<div className="mb-4">
-						<TableFilter placeholder="Search user" />
-					</div>
-					<CustomTable tableHeader={tableHeader} rowData={UsersListInfos} />
-					<div className="my-6">
-						<TablePagination />
-					</div>
-				</div>
-			</div>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <div className="pb-5 ml-4 bg-white rounded-lg">
+        <div className="rounded-xl">
+          <div className="mb-4">
+            <TableFilter placeholder="Search user" />
+          </div>
+          <CustomTable tableHeader={tableHeader} rowData={UsersListInfos} />
+          <div className="my-6">
+            <TablePagination />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default UsersList;
